94f0fda95c88f068d31dee4625123b7e4eb5394a
gbh10c <grandcasa10@gmail.com>
2022-02-10 00:25:40 -0500

    Beginning function examples

---
+++ 20-section/app.js
@@ -0,0 +1,41 @@
+// function singSong() {
+//     console.log("Do");
+//     console.log("Re");
+//     console.log("Mi");
+// }
+
+// function greet(firstName, lastName) {
+//     console.log(`Hi, ${firstName} ${lastName[0]}.!`);
+// }
+
+// greet('Laura', 'Hicken');
+
+// singSong();
+// singSong();
+// singSong();
+// singSong();
+// singSong();
+// singSong();
+// singSong();
+// singSong();
+
+// function repeat (message, times) {
+//     let fullMessage = '';
+//     for (let i = 0; i < times; i++) {
+//         fullMessage += message;
+//     }
+//     console.log(fullMessage)
+// }
+
+// repeat('$', 100)
+
+
+
+
+// function add(x, y) {
+//     if(typeof x !== 'number' || typeof y !== 'number') {
+//         return false;
+//     }
+//     return x + y;
+// }
+
