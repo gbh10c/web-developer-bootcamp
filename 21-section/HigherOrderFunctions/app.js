e440a746be1ee1493bd9ecda1e5e2f342583f248
gbh10c <grandcasa10@gmail.com>
2022-02-12 18:19:40 -0500

    Utilizing functions as arguments and returning functions - including factory functions.

---
+++ 21-section/HigherOrderFunctions/app.js
@@ -0,0 +1,65 @@
+// ======================
+// FUNCTIONS AS ARGUMENTS
+// ======================
+
+// function callTwice(func){
+//     func();
+//     func();
+// }
+
+// function callTenTimes(f){
+//     for(let i = 0; i < 10; i++) {
+//         f();
+//     }
+// }
+
+// function rollDie(){
+//     const roll = Math.ceil(Math.random() * 6);
+//     console.log(roll);
+// }
+
+// callTenTimes(rollDie);
+
+
+// ====================
+// RETURNING FUNCTIONS
+// ====================
+
+// function makeMysteryFunc(){
+//     const rand = Math.random();
+//     if (rand > 0.5){
+//         return function() {
+//             console.log('CONGRATS, I AM A GOOD FUNCTION!');
+//             console.log('YOU WIN A MILLION DOLLARS!');
+//         }
+//     } else {
+//         return function() {
+//             alert('YOU HAVE BEEN INVADED BY MEAN ALIENS');
+//             alert('STOP TRYING TO CLOSE THIS WINDOW');
+//             alert('STOP TRYING TO CLOSE THIS WINDOW');
+//             alert('STOP TRYING TO CLOSE THIS WINDOW');
+//             alert('STOP TRYING TO CLOSE THIS WINDOW');
+//             alert('STOP TRYING TO CLOSE THIS WINDOW');
+//             alert('STOP TRYING TO CLOSE THIS WINDOW');
+//         }
+//     }
+// }
+
+// const mystery = makeMysteryFunc();
+
+
+function makeBetweenFunc(min, max){
+    return function(num){
+        return num >= min && num <= max;
+    }
+}
+
+//makeBetweenFunc(50, 100) => 
+
+// function isBetween(num) {
+//     return num >= 50 && num <= 100;
+// }
+
+// function isBetween2(num) {
+//     return num >= 1 && num <= 10;
+// }
