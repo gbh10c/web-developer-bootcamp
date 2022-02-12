3a25d96d1f6c96ff48b15aa320216f1052d3c6a1
gbh10c <grandcasa10@gmail.com>
2022-02-12 17:46:05 -0500

    Introduces function expression

---
+++ 21-section/FunctionExpressions/app.js
@@ -0,0 +1,9 @@
+// First way we learned to create a function:
+// function add(x, y){
+//     return x + y;
+// }
+
+// Function expression (named by the variable)
+const add = function (x,y){
+    return x + y;
+}
