ff3f196b820734f66de0245fff4ad36edf26b39a
gbh10c <grandcasa10@gmail.com>
2022-02-13 12:23:15 -0500

    JavaScript Methods and shorthand

---
+++ 21-section/Methods/app.js
@@ -0,0 +1,18 @@
+const myMath = {
+    PI: 3.14159,
+    square: function(num){
+        return num * num;
+    },
+    cube: function(num){
+        return num ** 3;
+    },
+}
+
+const moreMath = {
+    square(num) {
+        return num * num;
+    },
+    cube(num){
+        return num ** 3;
+    }
+}
