1d787b8a53d30ed9d40e20f93dbcc1798a0ac72f
gbh10c <grandcasa10@gmail.com>
2022-02-13 12:44:19 -0500

    Methods, function shorthand inside object, and keyword 'this'

--- 21-section/Methods/app.js
+++ 21-section/Methods/app.js
@@ -1,18 +1,31 @@
-const myMath = {
-    PI: 3.14159,
-    square: function(num){
-        return num * num;
-    },
-    cube: function(num){
-        return num ** 3;
-    },
-}
+// const myMath = {
+//     PI: 3.14159,
+//     square: function(num){
+//         return num * num;
+//     },
+//     cube: function(num){
+//         return num ** 3;
+//     },
+// }
 
-const moreMath = {
-    square(num) {
-        return num * num;
-    },
-    cube(num){
-        return num ** 3;
+// const moreMath = {
+//     square(num) {
+//         return num * num;
+//     },
+//     cube(num){
+//         return num ** 3;
+//     }
+// }
+
+const cat = {
+    name: 'Milosch Hicken',
+    color: 'orange',
+    breed: 'short hair',
+    meow(){
+        console.log("THIS IS:", this)
+        console.log(`${this.name} says MEOWWWWW`);
     }
 }
+
+//"This" is not included because the function is invoked outside of meow
+const meow2 = cat.meow;
