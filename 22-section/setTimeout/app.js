5a3d02d1595d73f34be319fb5e81d1c994f060e2
gbh10c <grandcasa10@gmail.com>
2022-02-14 18:51:30 -0500

    setTimeout example including clearInterval

--- 22-section/setTimeout/app.js
+++ 22-section/setTimeout/app.js
@@ -1,13 +1,12 @@
-console.log("HELLO!!!...")
-setTimeout(() => {
-    console.log("...are you still there?")
-}, 3000)
+// console.log('Hello!!!')
+// setTimeout(() => {
+//     console.log('Are you still there?')
+// }, 3000)
 
-console.log("GOODBYE!!")
-
+// console.log('Goodbye!');
 
 const id = setInterval(() => {
     console.log(Math.random())
 }, 2000);
 
-// clearInterval(id);
+//use clearInterval(id) (or other variable name) to stop the function) 
