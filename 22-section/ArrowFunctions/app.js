45162e0e446c01176c9f6485e6cd29bb29edce63
gbh10c <grandcasa10@gmail.com>
2022-02-14 18:40:09 -0500

    Arrow functions and introduction to implicit return using arrow functions

--- 22-section/ArrowFunctions/app.js
+++ 22-section/ArrowFunctions/app.js
@@ -2,16 +2,12 @@
 //     return x + y;
 // }
 
-const add = (x,y) => {
-    return x + y;
-}
+// const add = (x,y) => {
+//     return x + y;
+// }
 
 const square = (x) => {
     return x * x;
-}
-
-const rollDie = () => {
-    return Math.ceil(Math.random() * 6);
 }
 
 const movies = [
@@ -33,14 +29,35 @@ const movies = [
     }
 ]
 
-const titles = movies.map((movie) => {
-    return movie.title;
-})
 
+// const films = movies.map(function(movie){
+//     return `${movie.title} - ${movie.score/10}/10`
+// })
 
-// IMPLICIT RETURN
+// With implicit return
+const films = movies.map(movie => (
+    `${movie.title} - ${movie.score/10}/10`
+))
 
+// Single line with implicit return
+// const films = movies.map(movie => `${movie.title} - ${movie.score/10}/10`);
 
+// IMPLICIT RETURN - one value very clearly expected to be returned
 
+// const rollDie = () => {
+//     return Math.ceil(Math.random() * 6);
+// }
 
+//Use parentheses to invoke implicit return
+const rollDie = () => (
+    Math.ceil(Math.random() * 6)
+)
 
+//For short functions, no parentheses and place on the same line
+const add = (a, b) => a + b
+
+// const isEven = (x) => (
+//     x % 2 === 0
+// )
+//or
+const isEven = (x) => x % 2 === 0
