5ae0cc441b32a9e7b0962c38059713aff0c8080c
gbh10c <grandcasa10@gmail.com>
2022-02-16 00:09:00 -0500

    Examples utilizing 'this' within arrow functions'

--- 22-section/ArrowFunctionsThis/app.js
+++ 22-section/ArrowFunctionsThis/app.js
@@ -1,14 +1,24 @@
 const person = {
     firstName: 'Viggo',
     lastName: 'Mortensen',
+    // fullName: () => {
+    //     return `${this.firstName} ${this.lastName}`
+    // } ***returns 'undefined undefined' because arrow function refers to window scope***
+    
     fullName: function () {
-        return `${this.firstName} ${this.lastName}`
-    },
-    shoutName: function () {
-        setTimeout(() => {
-            //keyword 'this' in arrow functions refers to the value of 'this' when the function is created
-            console.log(this);
-            console.log(this.fullName())
-        }, 3000)
-    }
+        return `${this.firstName} ${this.lastName}`;
+    }, //returns 'Viggo Mortensen'
+    
+    // shoutName: function() {
+    //     setTimeout(function() { 
+    //         console.log(this.fullName);
+    //     }, 3000);
+    // } ***returns error because setTimeout refers to window scope***
+    
+    shoutName: function() {
+        setTimeout(() => { 
+            console.log(`${this.fullName().toUpperCase()}!!!!!`);
+        }, 3000);
+    } //returns 'VIGGO MORTENSEN!!!!!' IF AND ONLY IF referring to a non-arrow function
+    // IF referring back to another arrow function ***returns 'undefined undefined' because arrow function refers to window scope***
 }
