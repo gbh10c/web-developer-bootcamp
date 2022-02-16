b718fb539cf69a3601d9e8f9d32bc48a0f52efa1
gbh10c <grandcasa10@gmail.com>
2022-02-15 22:59:10 -0500

    Some/Every examples

--- 22-section/SomeEvery/app.js
+++ 22-section/SomeEvery/app.js
@@ -1,6 +1,6 @@
-const exams = [80, 98, 92, 78, 77, 90, 89, 84, 81, 77]
+const exams = [80, 98, 92, 78, 70, 90, 89, 84, 81, 77]
 
-exams.every(score => score >= 75)
+const passed = exams.every(score => score >= 75)
 
 const movies = [
     {
@@ -50,5 +50,4 @@ const movies = [
     }
 ]
 
-
-movies.some(movie => movie.year > 2015)
+const newMovies = movies.some(movie => movie.year > 2010)
