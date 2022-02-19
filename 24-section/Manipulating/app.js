a6151f0879e17f14810f167e06becebdda1709d0
gbh10c <grandcasa10@gmail.com>
2022-02-19 00:10:33 -0500

    Basic DOM manipulation with innerText, innerHTML, and textContent, as well as getAttribue and setAttribute

---
+++ 24-section/Manipulating/app.js
@@ -0,0 +1,12 @@
+// const allLinks = document.querySelectorAll('a');
+
+// for(let link of allLinks){
+//     link.innerText = 'I AM A LINK!!!'
+// }
+
+//.innerHTML actually changes the markup through the DOM
+// document.querySelector('h1').innerHTML = '<i>Silkie Chickens</i>'
+
+// += adds to the existing element, while = overwrites everything
+// document.querySelector('h1').innerHTML += '<sup>asdo;fij</sup>'
+
