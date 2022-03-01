6cf1ec7cb163842b9546dfe421b5edc21f24e8b1
gbh10c <grandcasa10@gmail.com>
2022-03-01 00:08:24 -0500

    Constructor function example with rgb and hex colors, also rgba

--- 29-section/04_Constructor_Functions/app.js
+++ 29-section/04_Constructor_Functions/app.js
@@ -1,12 +1,12 @@
 // This is a Constructor Function...
-function Color(r, g, b) {
-	this.r = r;
-	this.g = g;
-	this.b = b;
-}
+// function Color(r, g, b) {
+// 	this.r = r;
+// 	this.g = g;
+// 	this.b = b;
+// }
 
 //If you call it on its own like a regular function...
-Color(35, 60, 190); //undefined
+// Color(35, 60, 190); undefined
 //It returns undefined. Seems useless!
 
 // *****************
@@ -18,22 +18,50 @@ Color(35, 60, 190); //undefined
 // 3. Passes the newly created object from Step 1 as the this context;
 // 4. Returns this if the function doesn't return its own object.
 
-Color.prototype.rgb = function() {
+function Color(r, g, b) {
+	this.r = r;
+	this.g = g;
+	this.b = b;
+}
+
+// new Color(40, 50, 60);
+
+Color.prototype.rgb = function () {
 	const { r, g, b } = this;
 	return `rgb(${r}, ${g}, ${b})`;
-};
+}
 
-Color.prototype.hex = function() {
+Color.prototype.hex = function () {
 	const { r, g, b } = this;
 	return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
 };
 
-Color.prototype.rgba = function(a = 1.0) {
+Color.prototype.rgba = function (a = 1.0) {
 	const { r, g, b } = this;
 	return `rgba(${r}, ${g}, ${b}, ${a})`;
-};
+}
 
-const color1 = new Color(40, 255, 60);
-color1.hex();
-const color2 = new Color(0, 0, 0);
-color2.hex();
+const color1 = new Color(40, 255, 60)
+const color2 = new Color(0, 0, 0)
+
+
+// PROVIDED EXAMPLES
+// Color.prototype.rgb = function() {
+// 	const { r, g, b } = this;
+// 	return `rgb(${r}, ${g}, ${b})`;
+// };
+
+// Color.prototype.hex = function() {
+// 	const { r, g, b } = this;
+// 	return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+// };
+
+// Color.prototype.rgba = function(a = 1.0) {
+// 	const { r, g, b } = this;
+// 	return `rgba(${r}, ${g}, ${b}, ${a})`;
+// };
+
+// const color1 = new Color(40, 255, 60);
+// color1.hex();
+// const color2 = new Color(0, 0, 0);
+// color2.hex();
