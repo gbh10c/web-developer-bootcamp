5e1a9b8ffc3ec21c04c6d291aa3b89e35adeeadf
gbh10c <grandcasa10@gmail.com>
2022-03-08 14:53:32 -0500

    Express example with get requests, .params, .query, and nodemon

--- 33-section/firstApp/index.js
+++ 33-section/firstApp/index.js
@@ -7,7 +7,19 @@ const app = express();
 // })
 
 app.get('/', (req, res) => {
-  res.send('This is the home page!')
+  res.send('This is the home page!!!');
+})
+
+// CREATE A ROUTE PATTERN
+// Using .params to get the term used in the request
+app.get('/r/:subreddit', (req, res) => {
+  const { subreddit } = req.params;
+  res.send(`<h1>Browsing the ${subreddit} subreddit!</h1>`);
+})
+
+app.get('/r/:subreddit/:postId', (req, res) => {
+  const { subreddit, postId } = req.params;
+  res.send(`<h1>Viewing Post ID: ${postId} on ${subreddit} subreddit!</h1>`);
 })
 
 app.post('/cats', (req, res) => {
@@ -22,13 +34,19 @@ app.get('/dogs', (req, res) => {
   res.send('WOOF!!!');
 })
 
+// Using .query to handle multiple key value pairs from a search
+app.get('/search', (req, res) => {
+  const { q } = req.query;
+  if (!q) {
+    res.send('<h1>Nothing found if nothing searched</h1>')
+  }
+  res.send(`<h1>You searched for ${q}.</h1>`);
+})
+
 app.get('*', (req, res) => {
   res.send(`I don't know that path!`)
 })
 
-// /cats => 'meow'
-// /dogs => 'woof'
-// '/'
 
 app.listen(8080, () => {
   console.log("LISTENING ON PORT 8080!")
