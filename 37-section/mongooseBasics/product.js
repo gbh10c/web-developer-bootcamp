3cea2310da6ea88dd7bb6ef0f78e232da0d8e90e
gbh10c <grandcasa10@gmail.com>
2022-03-13 00:21:06 -0500

    Mongoose validation errors, instance methods, and static methods.

--- 37-section/mongooseBasics/product.js
+++ 37-section/mongooseBasics/product.js
@@ -12,22 +12,87 @@ const productSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
+    maxlength: 20,
   },
   price: {
     type: Number,
     required: true,
+    min: [0, 'Price must be positive...duh.'],
   },
+  onSale: {
+    type: Boolean,
+    default: false,
+  },
+  categories: [String],
+  qty: {
+    online: {
+      type: Number,
+      default: 0,
+    },
+    inStore: {
+      type: Number,
+      default: 0,
+    },
+  },
+  size: {
+    type: String,
+    enum: ['S', 'M', 'L'],
+  }
 });
+
+// productSchema.methods.greet = function () {
+//   console.log('HELLO!!! HII!!! HOWDY!!!');
+//   console.log(`- from ${this.name}`)
+// }
+
+// Instance Methods
+productSchema.methods.toggleOnSale = function () {
+  this.onSale = !this.onSale;
+  return this.save();
+}
+
+productSchema.methods.addCategory = function(newCat) {
+  this.categories.push(newCat);
+  return this.save();
+}
+
+// Static Method
+productSchema.statics.fireSale = function () {
+  return this.updateMany({}, {onSale: true, price: 0})
+}
 
 const Product = mongoose.model('Product', productSchema);
 
-const bike = new Product({ name: 'Mountain Bike', price: 999, color: 'red' })
-bike.save()
-.then(data => {
-  console.log("IT WORKED!");
-  console.log(data);
-})
-.catch(err => {
-  console.log("ERROR: ");
-  console.log(err);
-})
+const findProduct = async () => {
+  const foundProduct = await Product.findOne({ name: 'Mountain Bike' })
+  console.log(foundProduct);
+  await foundProduct.toggleOnSale();
+  console.log(foundProduct);
+  await foundProduct.addCategory('Outdoors')
+  console.log(foundProduct);
+}
+
+Product.fireSale().then(res => console.log(res));
+
+// findProduct();
+
+// const bike = new Product({ name: 'Cycling Jersey', price: 28.50, categories: ['Cycling'], size: 'S' })
+// bike.save()
+//   .then(data => {
+//     console.log("IT WORKED!");
+//     console.log(data);
+//   })
+//   .catch(err => {
+//     console.log("ERROR: ");
+//     console.log(err);
+//   })
+
+// Product.findOneAndUpdate({ name: 'Tire Pump' }, { price: -9 }, {new: true, runValidators: true})
+//   .then(data => {
+//     console.log("IT WORKED!");
+//     console.log(data);
+//   })
+//   .catch(err => {
+//     console.log("ERROR: ");
+//     console.log(err);
+//   })
