244a0177fa238947cab32f58a44d92b45664b88d
gbh10c <grandcasa10@gmail.com>
2022-03-18 00:30:01 -0400

    Express error handling examples

---
+++ 42-section/expressErrors/index.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const AppError = require('./AppError');
+
+app.use(morgan('tiny'));
+
+app.use((req, res, next) => {
+  req.requestTime = Date.now();
+  console.log(req.method, req.path);
+  next();
+})
+
+app.use('/dogs', (req, res, next) => {
+  console.log("I LOVE DOGS");
+  next();
+})
+
+const verifyPassword = ((req, res, next) => {
+  const { password } = req.query;
+  if(password === 'chickennugget') {
+    next();
+  }
+  throw new AppError('Password required', 401)
+})
+
+app.get('/', (req, res) => {
+  console.log(`REQUEST DATE: ${req.requestTime}`)
+  res.send('HOME PAGE');
+})
+
+app.get('/error', (req, res) => {
+  chicken.fly();
+})
+
+app.get('/dogs', (req, res) => {
+  console.log(`REQUEST DATE: ${req.requestTime}`)
+  res.send('WOOF WOOF');
+})
+
+app.get('/secret', verifyPassword, (req, res) => {
+  res.send('SUPER SECRET SECRET!!!');
+})
+
+app.get('/admin', (req, res) => {
+  throw new AppError('You are not an Admin!', 403);
+})
+
+app.use((req, res) => {
+  res.status(404).send('NOT FOUND');
+})
+
+// app.use((err, req, res, next) => {
+//   console.log('***************************************************')
+//   console.log('*******************ERROR***************************')
+//   console.log('***************************************************')
+//   next(err);
+// })
+
+app.use((err, req, res, next) => {
+  const { status = 500, message = 'Something went wrong' } = err;
+  res.status(status).send(message);
+})
+
+app.listen(3000, () => {
+  console.log('App is running on localhost:3000');
+})
