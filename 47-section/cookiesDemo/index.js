2d9ab8687b8d1edc369f7d94ba3773d9265d2bf0
gbh10c <grandcasa10@gmail.com>
2022-03-26 22:09:34 -0400

    Cookies and signed cookies

--- 47-section/cookiesDemo/index.js
+++ 47-section/cookiesDemo/index.js
@@ -2,19 +2,30 @@ const express = require('express');
 const app = express();
 
 const cookieParser = require('cookie-parser');
-app.use(cookieParser());
+app.use(cookieParser('thisismysecret'));
 
 app.get('/greet', (req, res) => {
-  const { name = 'No-name'} = req.cookies;
+  const { name = 'No-name' } = req.cookies;
   res.send(`Hey there, ${name}!`)
 })
 
 app.get('/setname', (req, res) => {
   res.cookie('name', 'henrietta');
-  res.cookie('animal', 'harlequin shrimp')
-  res.send('OK, SENT YOU A COOKIE')
+  res.cookie('animal', 'harlequin shrimp');
+  res.send('OK, SENT YOU A COOKIE');
 })
 
-app.listen(3000, () =>{
+app.get('/getsignedcookie', (req, res) => {
+  res.cookie('fruit', 'grape', { signed: true });
+  res.send('OK, SIGNED YOUR FRUIT COOKIE')
+})
+
+app.get('/verifyfruit', (req, res) => {
+  console.log(req.cookies);
+  console.log(req.signedCookies);
+  res.send(req.signedCookies);
+})
+
+app.listen(3000, () => {
   console.log('SERVING ON 3000')
 })
